65bdfd2048f85d6704064a5a6359e158d4c7920a
Jeffrey Linwood <jlinwood@gmail.com>
2016-05-16 16:47:46 -0500

    Register bindings from the app to Twilio

--- index.rb
+++ index.rb
@@ -14,5 +14,26 @@ end
 post '/register' do
   # Authenticate with Twilio
   client = Twilio::REST::Client.new settings['TWILIO_ACCOUNT_SID'], settings['TWILIO_AUTH_TOKEN']
+  
+  # Reference a valid notification service
+  service = client.notifications.v1.services(settings['TWILIO_NOTIFICATION_SERVICE_SID'])
+  
+  begin 
+    binding = service.bindings.create(
+      endpoint: params[:endpoint],
+      identity: params[:identity],
+      binding_type: params[:BindingType],
+      address: params[:Address],
+    )
+    response = {message: 'Binding created!' }
+    response.to_json
+  rescue Twilio::REST::TwilioException => e
+    status 500
+      response = {
+        message: "Failed to create binding: #{e.message}",
+        error: e
+      }
+      response.to_json
+  end
 end
 
